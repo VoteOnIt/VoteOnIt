e475231b1f6b4c819a178ada49e24936282df373
Jacob Jenkins <jacob@jacobejenkins.com>
2015-03-13 23:58:23 -0400

    More things

--- app/routes/poll.server.routes.js
+++ app/routes/poll.server.routes.js
@@ -1,6 +1,12 @@
 'use strict';
 
 module.exports = function(app) {
-	// Routing logic   
-	// ...
+    var users = require('../../app/controllers/users.server.controller');
+    var polls = require('../../app/controllers/poll.server.controller');
+
+    app.route('/polls')
+        .get(polls.list)
+        .post(users.requiresLogin, polls.create);
+
+    app.param('pollId', polls.pollByID);
 };
